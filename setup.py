4ad166146119ede63d2dc04a4c0fc6293aed64dc
haiquanduan <haiquanduan@gmail.com>
2020-09-26 16:59:25 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='simple_ansible_api',
+    version='v1.0',
+    author='Haiquan',
+    author_email='haiquanduan@gmail.com',
+    description='simple_ansible_api',
+    license='MIT',
+    packages=['simple_ansible_api',],
+    keywords='ansible',
+    url='https://github.com/towithyou/simple_ansible_api',
+    zip_safe=True,
+    include_package_data=True,
+    platforms='any'
+)
